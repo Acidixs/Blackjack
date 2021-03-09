8e9760bc1030bca16b99187308d007c061081e21
Aleksander <Aleksanderbo10@gmail.com>
2021-03-09 21:12:24 +0100

    get winner of game

--- src/game.py
+++ src/game.py
@@ -32,8 +32,15 @@ class Blackjack:
     def hit_blackjack(self, score):
         return score == 21
 
+    def get_winner(self, playerScore, dealerScore):
+        if playerScore > dealerScore:
+            return "player"
+        elif dealerScore > playerScore:
+            return "dealer"
+
     def start(self):
         self.shuffle_cards()
+        card = self.draw_card
 
         dealerSum = self.get_dealer_hand
         playerSum = self.get_player_hand
@@ -44,32 +51,43 @@ class Blackjack:
         print("player:", playerSum())
         print("dealer:", dealerSum())
         
-        playing = True
+        playerTurn = True
 
-        while playing:
+        while playerTurn:
             choice = input("Hit or Stand: ")
             if choice == "Hit":
-                self.update_hand("player", self.draw_card())
+                self.update_hand("player", card())
                 print(playerSum())
                 if self.bust(playerSum()):
-                    print("Bust")
+                    print("Player Bust")
                     print("Dealer wins")
-                    playing = False
-                    break
+                    playerTurn = False
+                    return
                 elif self.hit_blackjack(playerSum()):
                     print("Player hit blackjack")
 
             elif choice == "Stand":
+                playerTurn = False
                 while dealerSum() < 17:
                     time.sleep(0.5)
-                    self.update_hand("dealer", self.draw_card())
+                    self.update_hand("dealer", card())
                     print("dealer:", dealerSum())
                     if self.bust(dealerSum()):
-                        print("Bust")
-                        playing = False
-                        break
+                        print("Dealer Bust")
+                        playerTurn = False
+                        return
                     elif self.hit_blackjack(dealerSum()):
                         print("Dealer hit blackjack")
+
+                winner = self.get_winner(playerSum(), dealerSum())
+                if winner == "player":
+                    print("Player wins!")
+                    return
+                elif winner == "dealer":
+                    print("Dealer wins!")
+                    return
+
+
 
 
 
